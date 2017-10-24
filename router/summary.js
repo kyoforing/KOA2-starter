ad09a5e8c73660415925215c37b8d238007a3ed1
kyoforing <kyoforing@gmail.com>
2017-10-24 17:38:47 +0800

    Issue: * implement sql execution Description:  implement sql execution Issue ID: -

--- router/summary.js
+++ router/summary.js
@@ -2,8 +2,17 @@
 
 const Format = require('../lib/format.js');
 const Router = require('koa-router');
+const knex = require('../config/knexConfig').read();
 const router = module.exports = new Router();
 
-router.get('/test', (ctx, next) => {
-    ctx.body = Format.success(null, 'HiHi', ctx);
+
+router.get('/test', async (ctx) => {
+    let countResult = await knex('useraccount').count('* as count')
+    .then((rows) => {
+        return rows[0];
+    }).catch(function(error) {
+        throw error;
+    });
+
+    ctx.body = Format.success(null, countResult, ctx);
 });
